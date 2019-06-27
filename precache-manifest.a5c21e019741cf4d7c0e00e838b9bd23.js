0bb158bba8d29641d48cf7b6d3fb469e4a1e49e1
jjczhao <jasonzhao15@gmail.com>
2019-06-27 01:26:37 -0700

    Updates

---
+++ precache-manifest.a5c21e019741cf4d7c0e00e838b9bd23.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a47649038043b62701116913dd66ac6",
+    "url": "/podomoro-clock/index.html"
+  },
+  {
+    "revision": "8a78532bb1e6ed9f6f55",
+    "url": "/podomoro-clock/static/css/main.28e6286f.chunk.css"
+  },
+  {
+    "revision": "6ef78d578034b20361ad",
+    "url": "/podomoro-clock/static/js/2.3e669e04.chunk.js"
+  },
+  {
+    "revision": "8a78532bb1e6ed9f6f55",
+    "url": "/podomoro-clock/static/js/main.4966a61d.chunk.js"
+  },
+  {
+    "revision": "b463efc1aa9cb5aa73c7",
+    "url": "/podomoro-clock/static/js/runtime~main.7c4366c2.js"
+  }
+]);
